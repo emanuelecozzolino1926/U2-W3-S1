8748d2156304dd5bcd55960b1e2c76ea3181ef23
Emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2025-11-03 15:49:25 +0100

    [UPD] es 2 terminated

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,52 @@ const user3 = new User("Michele", "Basile", 23, "Roma");
 console.log(user1.compareAge(user2));
 console.log(user2.compareAge(user1));
 console.log(user1.compareAge(user3));
+
+//FORM
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+
+  hasSameOwner(otherPet) {
+    if (this.ownerName.toLowerCase() === otherPet.ownerName.toLowerCase()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const pets = [];
+
+const form = document.getElementById("petForm");
+const petList = document.getElementById("petList");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const petName = document.getElementById("petName").value;
+  const ownerName = document.getElementById("ownerName").value;
+  const species = document.getElementById("species").value;
+  const breed = document.getElementById("breed").value;
+
+  const newPet = new Pet(petName, ownerName, species, breed);
+  pets.push(newPet);
+
+  const li = document.createElement("li");
+  li.innerText = `${newPet.petName} (${newPet.species}, ${newPet.breed}) - proprietario ${newPet.ownerName}`;
+  petList.appendChild(li);
+
+  pets.forEach((p) => {
+    if (p !== newPet && p.hasSameOwner(newPet)) {
+      console.log(
+        `${p.petName} e ${newPet.petName} hanno lo stesso proprietario ${newPet.ownerName}`
+      );
+    }
+  });
+
+  form.reset();
+});
